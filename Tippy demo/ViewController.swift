dbb4f0b7eb21899df9265c6e0da872cbbd46f455
Dominique Adapon <dadapon@fb.com>
2017-06-20 15:37:08 -0700

    Added dynamic tip and total

--- Tippy demo/ViewController.swift
+++ Tippy demo/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,13 +26,19 @@ class ViewController: UIViewController {
     }
 
 
-    @IBAction func onTap(_ sender: Any) {
+    @IBAction func onTap(_ sender: Any) { //same as AnyObject??
         view.endEditing(true)
     }
 
 
     @IBAction func calculateTip(_ sender: Any) {
         
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * 0.2
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
 
 }
