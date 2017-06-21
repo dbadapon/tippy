66a5ddbbe95e2776230016cd7d6cbf4bc39a1999
Dominique Adapon <dadapon@fb.com>
2017-06-20 20:06:31 -0700

    Made sure Settings worked

--- Tippy demo/SettingsViewController.swift
+++ Tippy demo/SettingsViewController.swift
@@ -33,7 +33,7 @@ class SettingsViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -41,6 +41,6 @@ class SettingsViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+ 
 
 }
--- Tippy demo/ViewController.swift
+++ Tippy demo/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool){
         super.viewWillAppear(animated)
+        print("view will appear")
         
         let defaults = UserDefaults.standard
         tipControl.selectedSegmentIndex = defaults.integer(forKey: "defaultIndex")
@@ -26,16 +27,19 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        print("view did appear")
 
     
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        print("view will disappear")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        print("view did disappear")
     }
 
     
